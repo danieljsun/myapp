8a0aaf7d9d7415426f4b144496eb3be42cbf812b
Daniel Sun <danieljsun@gmail.com>
2013-09-02 17:11:15 -0400

    npm install grunt-contrib-jade --save-dev; git ignore public; grunt jade config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,7 @@
+/* global module:false */
 module.exports = function(grunt) {
+  'use strict';
+  
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     browserify: {
@@ -7,8 +10,26 @@ module.exports = function(grunt) {
     watch: {
       files: [ "client/**/*.js"],
       tasks: [ 'browserify' ]
+    },
+    jade: {
+      compile: {
+        options: {
+          pretty: true,
+          data: {
+            debug: false
+          }
+        },
+        files: [{
+          expand: true,
+          cwd: 'client',
+          src: '{,*/}*.jade',
+          dest: 'public/',
+          ext: '.html'
+        }]
+      }
     }
-  })
-  grunt.loadNpmTasks('grunt-browserify')
-  grunt.loadNpmTasks('grunt-contrib-watch')
-}
+  });
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+};
