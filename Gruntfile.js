efe6f3f32bf7c987b384d2a562ee32fdec472a87
Daniel Sun <danieljsun@gmail.com>
2013-09-02 16:34:32 -0400

    npm install grunt-contrib-watch --save-de; init app.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,13 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     browserify: {
-      '/public/app.js': ['/client/app.js']
+      'public/app.js': ['client/app.js']
+    },
+    watch: {
+      files: [ "client/**/*.js"],
+      tasks: [ 'browserify' ]
     }
   })
   grunt.loadNpmTasks('grunt-browserify')
+  grunt.loadNpmTasks('grunt-contrib-watch')
 }
